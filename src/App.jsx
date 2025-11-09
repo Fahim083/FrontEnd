ab47abf873bd50367f7f551a63f749e6925e31e9
Fahim083 <fahimhaowlader07@gmail.com>
2025-11-09 22:19:33 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { Navigate, Route,RouterProvider,createBrowserRouter,createRoutesFromElements } from "react-router"
+
+import HomePage from "./Page/HomePage.jsx"
+import LoginPage from "./Page/LoginPage.jsx"
+import Register from "./Page/Register.jsx"
+
+function App() {
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/">
+      <Route index element={<Navigate to="/home" />} />
+      <Route path="home" element={<HomePage/>}/>
+      <Route path="login" element={<LoginPage/>}/>
+      <Route path="register" element={<Register/>}/>
+    </Route>
+
+
+  )
+)
+return <RouterProvider router={router}/>
+}
+
+export default App
---
+++ src/Page/LoginPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router'
+
+const LoginPage = () => {
+  return (
+    <div>
+        i am the login page
+    </div>
+  )
+}
+
+export default LoginPage
